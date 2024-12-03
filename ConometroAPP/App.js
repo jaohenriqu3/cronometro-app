655e2c41bb109570b727b7332bf7d35189dce369
jaohenriqu3 <jhlucena2004@gmail.com>
2024-12-03 17:27:35 -0300

    estruturando projeto

---
+++ ConometroAPP/App.js
@@ -0,0 +1,9 @@
+import Cronometro from './src/pages/cronometro.js';
+
+export default function App() {
+  return (
+    <Cronometro />
+  );
+}
+
+
---
+++ ConometroAPP/src/components/timer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { View } from 'react-native'; 
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Timer extends React.Component {
+
+
+
+}
